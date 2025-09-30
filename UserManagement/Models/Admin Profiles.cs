1c7f1e2e63e2890c6703dbdf6f6a3600bcd48669
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-09-30 16:12:14 +0400

    Represents the Admin Profile entity (table in the database)

---
+++ UserManagement/Models/Admin Profiles.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.Models
+{
+    // Represents the Admin Profile entity (table in the database)
+    public class Admin_Profiles
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // Manual entry for AdminId
+
+        public int AdminId { get; set; }
+    }
+}
