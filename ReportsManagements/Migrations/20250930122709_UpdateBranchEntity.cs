d1ad9714671d0367ebe5200654010f522331ad0e
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-30 18:36:32 +0400

    add migration

---
+++ ReportsManagements/Migrations/20250930122709_UpdateBranchEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportsManagements.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBranchEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                schema: "reports",
+                table: "Branches",
+                newName: "BranchId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BranchId",
+                schema: "reports",
+                table: "Branches",
+                newName: "Id");
+        }
+    }
+}
