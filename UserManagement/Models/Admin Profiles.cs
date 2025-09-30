df1833137d6d460097f0550c8b61cf32953d5d4d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-09-30 16:19:17 +0400

    Represents the Responsibility entity (table in the database)

--- UserManagement/Models/Admin Profiles.cs
+++ UserManagement/Models/Admin Profiles.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace UserManagement.Models
 {
     // Represents the Admin Profile entity (table in the database)
-    public class Admin_Profiles
+    public class Admin_Profile
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)] // Manual entry for AdminId
@@ -14,6 +14,14 @@ namespace UserManagement.Models
         // Navigation property for related responsibilities
         // One Admin can have many Responsibilities
         public ICollection<Responsibility> Responsibilitys {  get; set; }
+
+    }
+
+    // Represents the Responsibility entity (table in the database)
+    public class Responsibility
+    {
+        [ForeignKey("Admin_Profile")] // Declares AdminId as a Foreign Key linked to AdminProfile
+        public int AdminId { get; set; } // References the Admin who owns this Responsibility
 
 
     }
