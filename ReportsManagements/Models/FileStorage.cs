87090b647264c4630f64ea801f1dca31045ecb73
Thuraiya <3eya151@gmail.com>
2025-09-30 19:34:30 +0400

    edit in FileStorage

--- ReportsManagements/Models/FileStorage.cs
+++ ReportsManagements/Models/FileStorage.cs
@@ -6,12 +6,9 @@ namespace ReportsManagements.Models
     {
         [Key]
         public int FileStorageId { get; set; }
-        [Required]
         public string FileName { get; set; }
-        [Required]
         public string Url { get; set; }            
         public DateTime UploadedAt { get; set; }
-        [Required]
         public string UploadedBy { get; set; }
     }
 }
