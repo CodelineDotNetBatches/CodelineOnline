ca0afd692afea8dafd99ba14eb46726a5effa3da
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-30 14:19:04 +0400

    Create entities: Branch, Geolocation, BranchReport

---
+++ ReportsManagements/Models/Branch.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportsManagements.Models
+{
+    public class Branch
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }= true;
+    }
+}
---
+++ ReportsManagements/Models/BranchReport.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReportsManagements.Models
+{
+    public class BranchReport
+    {
+        [Key]
+        public int BranchReportId { get; set; }
+        public int TotalSessions { get; set; }
+        public int TotalStudents { get; set; }
+        public int AttendanceRate { get; set; }
+
+        // Foreign Key
+        [ForeignKey("Branch")]
+        public int BranchId { get; set; }
+    }
+}
