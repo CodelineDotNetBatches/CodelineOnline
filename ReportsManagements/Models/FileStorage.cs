64cb26c35201800ccc8f12380fbb5d9a8182d0d9
Thuraiya <3eya151@gmail.com>
2025-09-30 19:20:30 +0400

    create File storage and reason code models

--- ReportsManagements/Models/FileStorage.cs
+++ ReportsManagements/Models/FileStorage.cs
@@ -6,9 +6,12 @@ namespace ReportsManagements.Models
     {
         [Key]
         public int FileStorageId { get; set; }
+        [Required]
         public string FileName { get; set; }
+        [Required]
         public string Url { get; set; }            
         public DateTime UploadedAt { get; set; }
+        [Required]
         public string UploadedBy { get; set; }
     }
 }
--- ReportsManagements/Models/ReasonCode.cs
+++ ReportsManagements/Models/ReasonCode.cs
@@ -6,10 +6,12 @@ namespace ReportsManagements.Models
     {
         [Key]
         public int ReasonCodeId { get; set; }
+        [Required]
         public string Code { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Category { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
 
     }
 }
