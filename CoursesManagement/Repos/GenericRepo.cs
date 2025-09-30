4225467aec786028969f5d809e909f78517d1bf7
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-09-30 15:51:42 +0400

    Build Generic & IGenericRepo

---
+++ CoursesManagement/Repos/GenericRepo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursesManagement.Repos
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly CoursesDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(CoursesDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+}
