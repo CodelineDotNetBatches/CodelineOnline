66be82022666640978e033c693ae38ebed599b44
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-01 13:24:35 +0400

    Defines the base route -> API/AdminProfile

--- UserManagement/Controllers/AdminProfileController .cs
+++ UserManagement/Controllers/AdminProfileController .cs
@@ -2,7 +2,8 @@
 
 namespace UserManagement.Controllers
 {
-    [ApiController]    // Marks this class as an API controller 
+    [ApiController]                     // Marks this class as an API controller 
+    [Route("api/[controller]")]        // Defines the base route -> API/AdminProfile 
     public class AdminProfileController
     {
 
