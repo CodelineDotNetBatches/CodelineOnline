43170fb8100412c19748cc0e9bc2ef402ad10b90
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-01 15:03:05 +0400

    Unique idenifier Admin Profile data between layers (Service , Controller , etc)

--- UserManagement/DTOs/AdminProfileDTO.cs
+++ UserManagement/DTOs/AdminProfileDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace UserManagement.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.DTOs
 {
     // DTO for transferring Admin Profile data between layers (Service, Controll
     public class AdminProfileDTO
     {
+        // Unique idenifier Admin Profile data between layers (Service , Controller , etc)
 
+        [Required(ErrorMessage = "Id is required")]
+        public int Id { get; set; }
     }
 }
