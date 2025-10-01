3243264dc4551e5b649fae200a5628728cc51650
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-01 14:53:25 +0400

    Build repositories for TrainerReport & CourseReport

---
+++ ReportsManagements/Repositories/ICourseReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReportsManagements.Models;
+
+namespace ReportsManagements.Repositories
+{
+    public interface ICourseReportRepository
+    {
+        Task<CourseReport> AddAsync(CourseReport courseReport);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<CourseReport>> GetAllAsync();
+        Task<CourseReport?> GetByIdAsync(int id);
+        Task<CourseReport?> UpdateAsync(CourseReport courseReport);
+    }
+}
