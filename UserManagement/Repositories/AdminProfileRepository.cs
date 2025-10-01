97cbeed239fcdd3fac49cccc4c71e286cbc7dc77
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-01 12:06:01 +0400

    add  In-Memory Cache

--- UserManagement/Repositories/AdminProfileRepository.cs
+++ UserManagement/Repositories/AdminProfileRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace UserManagement.Repositories
+﻿using Microsoft.Extensions.Caching.Memory;
+using UserManagement.Models;
+
+namespace UserManagement.Repositories
 {
     public class AdminProfileRepository
     {
         private readonly UsersDbContext _context; // Database context
+        private readonly IMemoryCache _memoryCache;  // In-Memory Cache
 
 
         public AdminProfileRepository(UsersDbContext context) // Constructor injection of DbContext
@@ -11,6 +15,11 @@
         }
 
 
-
+        public IEnumerable<AdminProfile> GetAll()
+        {
+            return _context.AdminProfiles
+                           .Include(a => a.Responsibilities) // Eager load Responsibilities
+                           .ToList();
+        }
     }
 }
