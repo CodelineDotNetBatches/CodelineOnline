e8914279a1967d5d77506cf2edef4839976ef10a
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-01 13:21:31 +0400

    Marks this class as an API controller

---
+++ UserManagement/Controllers/AdminProfileController .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Controllers
+{
+    [ApiController]    // Marks this class as an API controller 
+    public class AdminProfileController
+    {
+
+
+    }
+}
