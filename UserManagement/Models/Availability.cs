16758335e36bd07a30cc9e29fb8ec29dc7c1419b
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-10-01 16:19:35 +0400

    availability + instructor Models

---
+++ UserManagement/Models/Availability.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace UserManagement.Models
+{
+    public class Availability
+    {
+        // Partial key with PK for for this weak entity
+        
+        public int avilabilityId { get; set; } // availabilityId with instructorId will be as composite key for this entity 
+        // Foreign Key to Instructor
+        [ForeignKey("Instructor")]
+        public int InstructorId { get; set; }
+
+        // Navigation property
+        public Instructor Instructor { get; set; }
+
+        public string Avail_Status { get; set; } // Active/ Inactive /Complete/Busy
+
+        public string day_of_week { get; set; } // Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+
+        public DateTime time { get; set; } // 12:00 AM/PM
+    }
+}
---
+++ UserManagement/Models/Instructor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace UserManagement.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int InstructorId { get; set; }
+        public string GithubUserName { get; set; }
+        public int Years_of_Experience { get; set; }
+        public string ProfileImage { get; set; }
+        public string InstructorCV { get; set; }
+        public string Experience_Level { get; set; }
+        public string Teaching_Style { get; set; }
+
+
+
+        // navigation property
+        public ICollection<Availability> Availabilities { get; set; }
+    }
+}
