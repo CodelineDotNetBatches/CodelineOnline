704d94d9e1f3df7bffd4a5b0920221c0cd5e3fbb
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-01 13:24:49 +0400

    Reference to service Layer

--- UserManagement/Controllers/AdminProfileController .cs
+++ UserManagement/Controllers/AdminProfileController .cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Services;
 
 namespace UserManagement.Controllers
 {
     [ApiController]                     // Marks this class as an API controller 
     [Route("api/[controller]")]        // Defines the base route -> API/AdminProfile 
-    public class AdminProfileController
+    public class AdminProfileController : ControllerBase
     {
+        private readonly AdminProfileService _service;  // Reference to service Layer 
 
 
     }
