d98abf6191fd0566dd5b2531e8073cb690da3ba6
rahmaAlmamari <alr816125@gmail.com>
2025-10-02 15:22:34 +0400

    Adding CourseUpdateDto DTO

---
+++ CoursesManagement/DTOs/CourseUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using CoursesManagement.Models;
+
+namespace CoursesManagement.DTOs
+{
+    public class CourseUpdateDto
+    {
+        public Guid CourseId { get; set; }
+        public string CourseName { get; set; } = default!;
+        public string? CourseDescription { get; set; }
+        public LevelType CourseLevel { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
