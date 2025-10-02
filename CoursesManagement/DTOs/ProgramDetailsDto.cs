779f08e3a60ed5d6eb3fe6417fd9b307d394b55f
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-10-02 15:26:21 +0400

    Add DTOs for program creation, details, and updates
    - Updated `ProgramCreateDto` with new properties and validation.
    - Created `ProgramDetailsDto` for detailed program representation.
    - Modified `ProgramUpdateDto` to include necessary properties and validation.

---
+++ CoursesManagement/DTOs/ProgramDetailsDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CoursesManagement.DTOs
+{
+    public class ProgramDetailsDto
+    {
+        public Guid ProgramId { get; set; }
+        public string ProgramName { get; set; } = null!;
+        public string? ProgramDescription { get; set; }
+        public string Roadmap { get; set; } = null!;
+        public DateTime CreatedAtUtc { get; set; }
+
+        
+        public List<Guid> CategoryIds { get; set; } = new();
+        public List<Guid> CourseIds { get; set; } = new();
+
+       
+        public List<string>? CategoryNames { get; set; }
+        public List<string>? CourseNames { get; set; }
+
+        public int EnrollmentsCount { get; set; }
+    }
+}
