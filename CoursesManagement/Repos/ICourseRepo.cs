66c3a6687f7a754cee9e80f2c53cbfd4f0bcfdbd
rahmaAlmamari <alr816125@gmail.com>
2025-10-02 14:56:22 +0400

    Extract ICourseRepo interface

---
+++ CoursesManagement/Repos/ICourseRepo.cs
@@ -0,0 +1,8 @@
+﻿using CoursesManagement.Models;
+
+public interface ICourseRepo
+{
+    Task<IQueryable<Course>> GetCoursesByCategoryAsync(int categoryId);
+    Task<IQueryable<Course>> GetCoursesByLevelAsync(LevelType level);
+    Task<Course?> GetCourseWithCategoryAsync(Guid courseId);
+}
