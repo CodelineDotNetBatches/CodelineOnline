b9ba038705f27d004c44f4e22c8d518e6869f675
Amani Khalid <amanikhalidd@gmail.com>
2025-10-02 15:39:27 +0400

    Created Certificate List Item Dto

---
+++ CoursesManagement/DTOs/CertificateListItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CoursesManagement.DTOs
+{
+    /// <summary>Compact list item for tables/search results.</summary>
+    public class CertificateListItemDto
+    {
+        public int CertificateId { get; set; }
+        public int CourseId { get; set; }
+        public int UserId { get; set; }
+        public string CertificateUrl { get; set; } = default!;
+        public DateTime IssuedAt { get; set; }
+    }
+}
