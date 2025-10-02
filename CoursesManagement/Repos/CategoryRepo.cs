ee9c810ffdfaba62d2500998edcc667772815271
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-10-02 15:18:09 +0400

    Fixes

--- CoursesManagement/Repos/CategoryRepo.cs
+++ CoursesManagement/Repos/CategoryRepo.cs
@@ -6,19 +6,15 @@ namespace CoursesManagement.Repos
     public class CategoryRepo : GenericRepo<Category>, ICategoryRepo
     {
         protected readonly CoursesDbContext _ctx;
-
         public CategoryRepo(CoursesDbContext context) : base(context)
         {
             _ctx = context;
         }
-
         public async Task<Category?> GetCategoryWithCoursesAsync(Guid id)
         {
             return await _dbSet
                 .Include(c => c.Courses)
                 .FirstOrDefaultAsync(c => c.CategoryId == id);
         }
-
-        public async Task SaveAsync() => await _context.SaveChangesAsync();
     }
 }
