f858b3044a75574b1b02da2832af48a07f64ca45
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-10-02 10:03:39 +0400

    DI

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -1,6 +1,12 @@
 
 using Microsoft.EntityFrameworkCore;
 using UserManagement;
+using UserManagement.Controllers.Middleware;
+using UserManagement.Repositories;
+using UserManagement.Services;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection; // for extension method
+using UserManagement.mapping;
 
 namespace CodeLine_Online
 {
@@ -13,14 +19,34 @@ namespace CodeLine_Online
             // Add services to the container.
             builder.Services.AddDbContext<UsersDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("Default"),
-            sql => sql.MigrationsHistoryTable("__Migrations_App"))); 
+            sql => sql.MigrationsHistoryTable("__Migrations_App")));
+
+            builder.Services.AddAutoMapper(
+                typeof(AvailabilityMappingProfile).Assembly,
+                typeof(InstructorMappingProfile).Assembly
+            );
+
+            //builder.Services.AddAutoMapper(typeof(Program));
+
+
+
+            builder.Services.AddScoped<IInstructorRepository, InstructorRepository>();
+            builder.Services.AddScoped<IAvailabilityRepository, AvailabilityRepository>();
+            builder.Services.AddScoped<IInstructorService, InstructorService>();
+            builder.Services.AddScoped<IAvailabilityService, AvailabilityService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddMemoryCache();
+            builder.Services.AddTransient<ErrorHandlingMiddleware>();
+
             var app = builder.Build();
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
