c7fb9a56a1b0a321d2a0848f88441e91bd7799ec
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-10-02 14:18:26 +0400

    add DTOs of users and roles

---
+++ AuthenticationManagement/DTOs/RoleDto.cs
@@ -0,0 +1,21 @@
+﻿namespace AuthenticationManagement.DTOs
+{
+    public class RoleDto
+    {
+        public int RoleID { get; set; }
+        public string RoleType { get; set; } = default!;
+        public string? Description { get; set; }
+    }
+
+    public class CreateRoleDto
+    {
+        public string RoleType { get; set; } = default!;
+        public string? Description { get; set; }
+    }
+
+    public class UpdateRoleDto
+    {
+        public string RoleType { get; set; } = default!;
+        public string? Description { get; set; }
+    }
+}
