442fa5f417fa964cd5709911837a313eca706b07
MOHAMMED <mohaalialalawi20@gmail.com>
2025-10-02 14:35:24 +0400

    add Dtos

---
+++ ReportsManagements/DTOs/ReportDtos.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportsManagements.DTOs
+{
+    // Trainer Report DTO
+    public record TrainerReportUpsertDto(
+    string TrainerId, string CourseId,
+    int TotalSessions, int TotalStudents, double AttendanceRate);
+
+    // Course Report DTO
+    public record CourseReportUpsertDto(
+        string CourseId, int TotalSessions, int TotalStudents, double AverageAttendanceRate);
+}
