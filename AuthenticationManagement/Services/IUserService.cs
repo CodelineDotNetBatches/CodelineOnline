f176ce4d4925ecb795a2c09cbf612c7e42287acd
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-10-02 14:48:05 +0400

    add services for user and role

---
+++ AuthenticationManagement/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using AuthenticationManagement.DTOs;
+
+namespace AuthenticationManagement.Services
+{
+    public interface IUserService
+    {
+        Task<UserDto> CreateAsync(CreateUserDto dto);
+        Task<bool> DeleteAsync(int id);
+        Task<IEnumerable<UserDto>> GetAllAsync();
+        Task<UserDto?> GetByIdAsync(int id);
+        Task<UserDto?> UpdateAsync(int id, UpdateUserDto dto);
+    }
+}
