b0d28ac293c610b2cf3eafc7620983b9d8117f56
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-10-05 15:30:45 +0400

    Add IProgramsService interface for program management

---
+++ CoursesManagement/Services/IProgramsService.cs
@@ -0,0 +1,13 @@
+﻿using CoursesManagement.DTOs;
+
+namespace CoursesManagement.Services
+{
+    public interface IProgramsService
+    {
+        Task<ProgramDetailsDto> CreateProgramAsync(ProgramCreateDto dto);
+        Task DeleteProgramAsync(Guid id);
+        Task<IEnumerable<ProgramDetailsDto>> GetAllProgramsAsync();
+        Task<ProgramDetailsDto?> GetProgramByIdAsync(Guid id);
+        Task UpdateProgramAsync(Guid id, ProgramUpdateDto dto);
+    }
+}
