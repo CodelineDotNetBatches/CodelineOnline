0d81adb6ff077ead378a52477018d6c287f239fd
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-05 09:56:32 +0400

    edit in GeoValidationService

--- ReportsManagements/Services/GeoValidationService .cs
+++ ReportsManagements/Services/GeoValidationService .cs
@@ -1,6 +1,6 @@
 ﻿namespace ReportsManagements.Services
 {
-    public class GeoValidationService
+    public class GeoValidationService : IGeoValidationService
     {
         //Main method to check if a point is within a circle
         public bool IsPointInsideCircle(double PoinLat, double PointLon, double CenterLat, double CenterLon, double RadiusMeters, out double DistanceMeters)//use out to return an extra value alongside the boolean result.
@@ -11,13 +11,13 @@
             return DistanceMeters <= RadiusMeters;
         }
         //private helpers methods to calculate Haversine distance between two points on erth using formula
-        private double HaversineDistance(double Lat1,double lon1,double Lat2,double Lon2)
+        private double HaversineDistance(double Lat1, double lon1, double Lat2, double Lon2)
         {
             double R = 6371000; // Radius of the Earth in meters
             double dLat = DegreesToRadians(Lat2 - Lat1);//difference in latitude in radians
             double dLon = DegreesToRadians(Lon2 - lon1);// difference in longitude in radians
             //Haversine formula
-            var a =Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
                     Math.Cos(DegreesToRadians(Lat1)) * Math.Cos(DegreesToRadians(Lat2)) *
                     Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
 
