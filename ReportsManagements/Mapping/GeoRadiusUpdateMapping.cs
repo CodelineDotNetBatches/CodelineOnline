753f12ddb9d8c4868b00120fc38d26f4a9af4535
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-06 12:57:51 +0400

    Edit Mapping

---
+++ ReportsManagements/Mapping/GeoRadiusUpdateMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ReportsManagements.DTOs;
+using ReportsManagements.Models;
+
+namespace ReportsManagements.Mapping
+{
+    public class GeoRadiusUpdateMapping: Profile
+    {
+        public GeoRadiusUpdateMapping() 
+        {
+            CreateMap<GeolocationDTO.GeoRadiusUpdateDto, Geolocation>()
+               .ForMember(dest => dest.RediusMeters, opt => opt.MapFrom(src => (decimal)src.NewRadius));
+        }
+    }
+}
---
+++ ReportsManagements/Mapping/GeolocationMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ReportsManagements.DTOs;
+using ReportsManagements.Models;
+
+namespace ReportsManagements.Mapping
+{
+    public class GeolocationMapping: Profile
+    {
+        public GeolocationMapping()
+        {
+            CreateMap<GeolocationDTO.GeolocationCreateDto, Geolocation>();
+            CreateMap<Geolocation, GeolocationDTO.GeolocationResponseDto>();
+        }
+    }
+}
