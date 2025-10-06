6c0c442daa0cfd0fa5af4360fce1a54263e34ee9
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-10-06 10:41:44 +0400

    create instructor and trainee skills

---
+++ UserManagement/Models/InstructorSkill.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class InstructorSkill
+    {
+        //[Key]
+        public int InstructorSkillId { get; set; }
+        [Required]
+        public string SkillName { get; set; }
+        public string? SkillLevel { get; set; }
+        public int? MonthsOfExperience { get; set; }
+        // Foreign Key reference to the user
+        
+        [ForeignKey("Instructor")]
+        public int InstructorId { get; set; }
+
+       
+
+        // Navigation property
+        public Instructor Instructor { get; set; } // one skill belongs to one instructor
+    }
+
+}
