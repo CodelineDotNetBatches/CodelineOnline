db2ad374c071cfe00e528f36889c97e93ee91c2c
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-06 14:51:10 +0400

    true if operation succeeded

---
+++ UserManagement/Helpers/Helpers.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.Helpers
+{
+    // Generic response wrapper for consistent API output.
+    // Used by Controllers and Services.
+    public class Helpers
+    {
+        public class ApiResponse<T>
+        {
+            public bool Success { get; set; } //true if operation succeeded
+        }
+}
