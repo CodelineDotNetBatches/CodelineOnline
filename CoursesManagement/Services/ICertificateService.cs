25bdf9d54647a0326f681ff8abd76eec0c697249
Amani Khalid <amanikhalidd@gmail.com>
2025-10-06 11:08:05 +0400

    Extract ICertificateService

---
+++ CoursesManagement/Services/ICertificateService.cs
@@ -0,0 +1,16 @@
+﻿using CoursesManagement.DTOs;
+
+namespace CoursesManagement.Services
+{
+    public interface ICertificateService
+    {
+        Task DeleteAsync(Guid certificateId);
+        Task<CertificateDetailsDto?> GetByIdAsync(Guid certificateId);
+        Task<CertificateDetailsDto> IssueAsync(CertificateIssueDto dto);
+        Task<List<CertificateListItemDto>> ListByUserAsync(Guid userId);
+       // Task<PagedResult<CertificateListItemDto>> SearchAsync(CertificateQueryDto query);
+        Task UpdateUrlAsync(Guid certificateId, CertificateUpdateUrlDto dto);
+        Task<CertificateVerifyResultDto> VerifyByEnrollmentAsync(Guid enrollmentId);
+        Task<CertificateVerifyResultDto> VerifyByUrlAsync(string certificateUrl);
+    }
+}
