4f75690352cc687972b5d2e6a21968c2dd10d2d6
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-07 13:13:29 +0400

    Primary Key for the Branch table

---
+++ UserManagement/Models/Branch.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.Models
+{
+    public class Branch
+    {
+        [Key]
+      
+        public int BranchId { get; set; } // Primary Key for the Branch table
+
+    }
+}
