36411d8f450c3f3de2111a68374ff8119433fefe
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-07 13:14:12 +0400

    The country where the branch is located

--- UserManagement/Models/Branch.cs
+++ UserManagement/Models/Branch.cs
@@ -13,5 +13,9 @@ namespace UserManagement.Models
         [StringLength(100, ErrorMessage = "City name cannot exceed 100 characters.")]
         public string City { get; set; } // The city where the branch is located
 
+        [Required(ErrorMessage = "Country is required.")]
+        [StringLength(100, ErrorMessage = "Country name cannot exceed 100 characters.")]
+        public string Country { get; set; } //The country where the branch is located
+
     }
 }
