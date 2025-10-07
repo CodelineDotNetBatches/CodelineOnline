c857f2e402da84c658daf4fcd3008ba10d027bed
rahmaAlmamari <alr816125@gmail.com>
2025-10-07 15:19:07 +0400

    Modify ICourseService

--- CoursesManagement/Services/ICourseService.cs
+++ CoursesManagement/Services/ICourseService.cs
@@ -6,7 +6,7 @@ namespace CoursesManagement.Services
     public interface ICourseService
     {
         Task<IEnumerable<Course>> GetAllCoursesAsync();
-        Task<Course?> GetCourseByIdAsync(Guid id);
+        Task<CourseListDto?> GetCourseByIdAsync(Guid id);
         Task<IEnumerable<Course>> GetCoursesByLevelAsync(LevelType level);
         Task<IEnumerable<Course>> GetCoursesByCategoryAsync(Guid categoryId);
         Task<Course?> GetCourseWithCategoryAsync(Guid id);
