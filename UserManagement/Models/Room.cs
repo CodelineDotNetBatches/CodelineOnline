1f03895778c21e6e6f8a7b660a9c44530ebda3aa
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-10-07 14:17:46 +0400

    Unique room number

---
+++ UserManagement/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class Room
+    {
+
+        [Required(ErrorMessage = "Room number is required.")]
+        [StringLength(10, ErrorMessage = "Room number cannot exceed 10 characters.")]
+        public string RoomNumber { get; set; } // Unique room number 
+
+
+    }
+}
