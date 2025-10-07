0741159b0d471295d7d4d7b5803933699ae5617d
rahmaAlmamari <alr816125@gmail.com>
2025-10-07 15:45:30 +0400

    Modify CourseUpdateDto

--- CoursesManagement/DTOs/CourseUpdateDto.cs
+++ CoursesManagement/DTOs/CourseUpdateDto.cs
@@ -9,6 +9,6 @@ namespace CoursesManagement.DTOs
         public string? CourseDescription { get; set; }
         public LevelType CourseLevel { get; set; }
         public decimal Price { get; set; }
-        public int CategoryId { get; set; }
+        public Guid CategoryId { get; set; }
     }
 }
